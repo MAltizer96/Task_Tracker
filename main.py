9a5a55877f31e0269a3a252c345047d0370b10e6
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2024-06-30 14:19:13 -0400

    updated main.py - added connection for about to close - added on_app_exit method

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow
 import threading
 
 from view import MainWindow
 from model import ProductiveModel
 
-class ProductivityApp:
+class ProductivityApp():
     def __init__(self):
         self.model = ProductiveModel()
         self.view = MainWindow()
+        # Connect the about_to_close signal to the on_app_exit slot        
+        self.view.about_to_close.connect(self.on_app_exit)
 
         # Connect signals and slots
         self.view.start_button.clicked.connect(self.start_tracking)
@@ -19,9 +21,11 @@ class ProductivityApp:
         if not self.model.is_tracking:
             self.tracking_thread = threading.Thread(target=self.model.start_tracking)
             self.tracking_thread.start()
+            self.view.start_tracking()
 
     def stop_tracking(self):
         self.model.stop_tracking()
+        self.view.stop_tracking()
 
     def submit_to_db(self):
         self.model.write_daily_activity_to_db()
@@ -30,6 +34,15 @@ class ProductivityApp:
         data = self.model.fetch_data()
         print(f'data: {data}')
         self.view.setup_pie_chart(data)
+
+
+    def on_app_exit(self):
+        
+
+        print('stopping tracking')
+        self.model.stop_tracking()
+        self.tracking_thread.join()
+
 
 app = QApplication([])
 productivity_app = ProductivityApp()

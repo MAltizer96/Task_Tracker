bf9641f3a394de0456eca3f86a3f1770c3d85938
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2024-06-30 12:49:30 -0400

    added create_pie_chart method and button to link view to model

--- main.py
+++ main.py
@@ -13,19 +13,23 @@ class ProductivityApp:
         self.view.start_button.clicked.connect(self.start_tracking)
         self.view.stop_button.clicked.connect(self.stop_tracking)
         self.view.submit_to_db_button.clicked.connect(self.submit_to_db)
+        self.view.visualize_button.clicked.connect(self.create_pie_chart)
 
     def start_tracking(self):
-        print('Starting tracking')
         if not self.model.is_tracking:
             self.tracking_thread = threading.Thread(target=self.model.start_tracking)
             self.tracking_thread.start()
 
     def stop_tracking(self):
-        print('Stopping tracking')
         self.model.stop_tracking()
 
     def submit_to_db(self):
         self.model.write_daily_activity_to_db()
+    
+    def create_pie_chart(self):
+        data = self.model.fetch_data()
+        print(f'data: {data}')
+        self.view.setup_pie_chart(data)
 
 app = QApplication([])
 productivity_app = ProductivityApp()

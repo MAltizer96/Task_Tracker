10b1f5475c8b6aff5c54d22504760874bc001dc5
Krinin96 <78855790+Krinin96@users.noreply.github.com>
2024-06-30 12:50:52 -0400

    updated model.py - updated write_daily_activity_to_db method to now write uncategorized to a csv file instead of db - added fetch_data method - added calculate_activities_time_in_minutes method

--- model.py
+++ model.py
@@ -42,7 +42,12 @@ class ProductiveModel:
             # reads the csv file and checks the type of activity
             for column in csv_reader:   
                 # check the type of activity             
-                activity_type = self.check_type_of_activity(column[1])        
+                activity_type = self.check_type_of_activity(column[1])     
+                if activity_type == 'uncategorized':                     
+                    with open('uncategorized_activities.csv', mode='a', newline='') as uncategorized_file:
+                        uncategorized_writer = csv.writer(uncategorized_file)
+                        uncategorized_writer.writerow([column[0], column[1]])                      
+                    continue
                 # check if the activity is in the db       
                 cursor.execute('SELECT * FROM activity_log WHERE type = ?', (activity_type,))
                 result = cursor.fetchone()
@@ -53,6 +58,7 @@ class ProductiveModel:
                     # if the activity is not in the db, insert the activity
                     cursor.execute('INSERT INTO activity_log VALUES (?, ?)', (activity_type, 1))
             # commit and close the connection
+            print('done')
             connection.commit()
             connection.close()
             
@@ -71,3 +77,19 @@ class ProductiveModel:
                 return 'entertainment'
         
         return 'uncategorized'
+    
+    def fetch_data(self):
+        # open connection to the db
+        connection = sqlite3.connect('activity_log.db')
+        cursor = connection.cursor()
+        # get the count of work and entertainment activities
+        work_activities_count = cursor.execute('SELECT count FROM activity_log WHERE type = "work"').fetchone()
+        entertainment_activities_count = cursor.execute('SELECT count FROM activity_log WHERE type = "entertainment"').fetchone()
+        # calculate the time spent on work and entertainment activities in minutes
+        work_activity_time = self.calculate_activities_time_in_minutes(work_activities_count[0])       
+        entertainment_activity_time = self.calculate_activities_time_in_minutes(entertainment_activities_count[0])
+        return {'work': work_activity_time, 'entertainment': entertainment_activity_time}
+   
+    def calculate_activities_time_in_minutes(self, activity_count):
+        return activity_count * 5 / 60 
+    
